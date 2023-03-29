a464ec9dcc7131395390d01cf2efdb5d15133eef
kibetamos <amos.rotich@strathmore.edu>
2023-03-29 19:12:54 +0300

    added psotgress sb

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coredb',
+        'USER': 'core',
+            'PASSWORD': 'wCh29&HE&T83',
+       'HOST': 'localhost',
+       'PORT': '5432',
     }
 }
 
