147f4b5a1819dc9191ad9c6c68c58ed34ade17a3
kibetamos <amos.rotich@strathmore.edu>
2023-03-29 19:43:23 +0300

    added user models

--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.http import Http404
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+  public_id = models.UUIDField(db_index=True, unique=True,default=uuid.uuid4, editable=False)
+  username = models.CharField(db_index=True,max_length=255, unique=True)
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
