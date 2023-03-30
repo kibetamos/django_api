4db5ec7c1676f097241c8d150039f4427a88d5db
kibetamos <amos.rotich@strathmore.edu>
2023-03-30 19:00:14 +0300

    created routers.py file

--- core/user/models.py
+++ core/user/models.py
@@ -74,7 +74,7 @@ class User(AbstractBaseUser, PermissionsMixin):
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['username']
   
-  objects     =   UserManager()
+  objects = UserManager()
   
   def __str__(self):
     return f"{self.email}"
