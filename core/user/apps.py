a9ae43a6c4ee75314d8b28e98ddf6d15e6b361a2
kibetamos <amos.rotich@strathmore.edu>
2023-03-30 18:31:45 +0300

    updated models

--- core/user/apps.py
+++ core/user/apps.py
@@ -1,12 +1,11 @@
 from django.apps import AppConfig
 
 
+
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.user'
     label = 'core_user'
+# Let's register the application now:
     'core',
     'core.user'
-    
-    
-    
--- core/user/models.py
+++ core/user/models.py
@@ -9,33 +9,7 @@ from django.db import models
 from django.http import Http404
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-  public_id = models.UUIDField(db_index=True, unique=True,default=uuid.uuid4, editable=False)
-  username = models.CharField(db_index=True,max_length=255, unique=True)
-  first_name = models.CharField(max_length=255)
-  
-  last_name = models.CharField(max_length=255)
-  email = models.EmailField(db_index=True, unique=True)
-  is_active = models.BooleanField(default=True)
-  is_superuser = models.BooleanField(default=False)
-  created = models.DateTimeField(auto_now=True)
-  updated = models.DateTimeField(auto_now_add=True)
-  
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['username']
-  
-  objects = BaseUserManager()
-  
-  def __str__(self):
-    return f"{self.email}"
-  
-  
-@property
-def name(self):
-   return f"{self.first_name} {self.last_name}"
- 
- 
- 
+
 class UserManager(BaseUserManager):
   def get_object_by_public_id(self, public_id):
     try:
@@ -84,6 +58,31 @@ class UserManager(BaseUserManager):
     
     return user
   
-    
+
+class User(AbstractBaseUser, PermissionsMixin):
+  public_id = models.UUIDField(db_index=True, unique=True,default=uuid.uuid4, editable=False)
+  username = models.CharField(db_index=True,max_length=255, unique=True)
+  first_name = models.CharField(max_length=255)
+  
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(db_index=True, unique=True)
+  is_active = models.BooleanField(default=True)
+  is_superuser = models.BooleanField(default=False)
+  created = models.DateTimeField(auto_now=True)
+  updated = models.DateTimeField(auto_now_add=True)
+  
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
+  
+  objects     =   UserManager()
+  
+  def __str__(self):
+    return f"{self.email}"
+  
+  
+@property
+def name(self):
+   return f"{self.first_name} {self.last_name}"
  
-    
+ 
+ 
