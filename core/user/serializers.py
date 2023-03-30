05368f5f3bb47c1b3cbead3f6533961e59df181a
kibetamos <amos.rotich@strathmore.edu>
2023-03-30 20:28:49 +0300

    update serializer and add the viewset

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -11,8 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
   
   class Meta:
     model = User
-    fields = ['id', 'username', 'first_name','last_name', 'bio', 'avatar', 'email',
-      'is_active', 'created', 'updated']
+    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_active','created', 'updated']
   read_only_field = ['is_active']
  
  
