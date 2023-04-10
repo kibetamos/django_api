6cda1dbbf9411a92cc5f0f3fbb6a5bcccfef8fb7
kibetamos <amos.rotich@strathmore.edu>
2023-04-10 09:09:51 +0300

    updated the models file

--- core/user/models.py
+++ core/user/models.py
@@ -1,22 +1,10 @@
-import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
 from core.abstract.models import AbstractModel, AbstractManager
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
-from django.http import Http404
 
-
-# class UserManager(BaseUserManager):
 class UserManager(BaseUserManager, AbstractManager):
-    def get_object_by_public_id(self, public_id):
-
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
 
     def create_user(self, username, email, password=None, **kwargs):
         """Create and return a `User` with an email, phone number, username and password."""
@@ -52,9 +40,7 @@ class UserManager(BaseUserManager, AbstractManager):
         return user
 
 
-# class User(AbstractBaseUser, PermissionsMixin):
 class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -66,8 +52,10 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True)
 
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    posts_liked = models.ManyToManyField(
+        "core_post.Post",
+        related_name="liked_by"
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -80,3 +68,15 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def like(self, post):
+        """Like `post` if it hasn't been done yet"""
+        return self.posts_liked.add(post)
+
+    def remove_like(self, post):
+        """Remove a like from a `post`"""
+        return self.posts_liked.remove(post)
+
+    def has_liked(self, post):
+        """Return True if the user has liked a `post`; else False"""
+        return self.posts_liked.filter(pk=post.pk).exists()
