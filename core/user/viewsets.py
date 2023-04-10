4da8eca2719d2adf7a56f66a15ce181adeeb7c99
kibetamos <amos.rotich@strathmore.edu>
2023-04-10 09:32:46 +0300

    created post app

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -2,10 +2,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
 from core.user.serializers import UserSerializer
+from core.abstract.viewsets import AbstractViewSet
 from core.user.models import User
 
-
-class UserViewSet(viewsets.ModelViewSet):
+# class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(AbstractViewSet):
     http_method_names = ('patch', 'get')
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
