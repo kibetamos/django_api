abcc9cb0097b2a558d76e209cbffbfbfdf3f767f
kibetamos <amos.rotich@strathmore.edu>
2023-04-10 09:16:26 +0300

    added the searlizers file

---
+++ core/abstract/serializers.py
@@ -0,0 +1,12 @@
+from core.abstract.serializers import AbstractSerializer
+from core.user.models import User
+
+
+class UserSerializer(AbstractSerializer):
+    class Meta:
+        model = User
+        # List of all the fields that can be included in a request or a response
+        fields = ['id', 'username', 'first_name', 'last_name', 'bio', 'avatar', 'email', 'is_active',
+                  'created', 'updated']
+        # List of all the fields that can only be read by the user
+        read_only_field = ['is_active']
