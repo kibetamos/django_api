a049f211d4610a6fa6e746e4d4e85014f515359e
kibetamos <amos.rotich@strathmore.edu>
2023-04-13 17:08:03 +0300

    addedthe models field

--- post/models.py
+++ post/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from core.abstract.models import AbstractModel,AbstractManager
+
+class PostManager(AbstractManager):
+  pass
+
+class Post(AbstractManager):
+  author = models.ForeignKey(to="core_user.User",on_delete=models.CASCADE)
+  
+  body = models.TextField()
+  
+  edited = models.BooleanField(default=False)
+  
+  objects = PostManager()
+  
+  def __str__(self):
+    return f"{self.author.name}"
+  
+  
+  class Meta:
+    db_table = "'core.post'"
