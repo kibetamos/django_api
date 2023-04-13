e1023ea886b5b5a6bd90ba34d9a2a48e9e26819c
kibetamos <amos.rotich@strathmore.edu>
2023-04-13 17:04:44 +0300

    uo

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
 from core.user.models import User
+from core.abstract.serializers import AbstractSerializer
+from core.user.models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(AbstractSerializer):
     # Rewriting some fields like the public id to be represented as the id of the object
     id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
     created = serializers.DateTimeField(read_only=True)
